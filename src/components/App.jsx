fb178909ebff51aadfc78da70788cfc2dd2af3c7
Ivan Kravchenko <kravchenkovano04@gmail.com>
2023-03-01 15:44:55 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
+import user from "../Data/user.json";
+import data from "../Data/data.json";
+import friends from "../Data/friends.json";
+import transactions from "../Data/transactions.json";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template22
-    </div>
+    <>
+   <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
